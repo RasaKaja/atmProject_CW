f8df91f83c42564d8f02e8b85a1f745c42e40382
Rasa <rasa.kajackaite@gmail.com>
2022-06-23 10:32:56 +0300

    project

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import controller.AtmController;
+import controller.BankController;
+import dto.User;
+import dto.UserCard;
+
+import javax.swing.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankController bankController = new BankController();
+
+        Date expiryDate = new Date(2022, Calendar.JUNE, 22);
+//        try {
+//            expiryDate = new SimpleDateFormat("yyyy-mm-dd").parse("2022-06-22");
+//        } catch (Exception ee) {
+//            System.out.println(ex);;
+//        }
+
+        UserCard card = new UserCard(expiryDate, 1245.50d, 2022);
+        User user1 = new User("Rasa", "123456789", card, 2.15);
+
+        bankController.createUser(user1);
+
+        AtmController atmController = new AtmController();
+        atmController.setBankController(bankController);
+        atmController.insertCard(user1.getCard());
+
+        JOptionPane.showMessageDialog(null, atmController.deposit());
+        JOptionPane.showMessageDialog(null, atmController.withdraw());
+
+        JOptionPane.showMessageDialog(null, atmController.validateCard()); // parentComponent: is it in separate open window, GUI window, some background thing
+
+        JOptionPane.showMessageDialog(null, atmController.deposit());
+        JOptionPane.showMessageDialog(null, atmController.withdraw());
+        JOptionPane.showMessageDialog(null, atmController.removeCard());
+
+        JOptionPane.showMessageDialog(null, atmController.withdraw());
+    }
+}
